254766ab9329f33af272cf7dc167ebd2923c7271
MuhammadYeasinArafath <mdyeasinarafathofficial@gmail.com>
2022-08-19 02:35:58 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# -Hello-World-in-Go
+“Hello, World!” in Go lang.
+<br>
+you can run this go code in your terminal writing [go run src\main\main.go]. 
+<br>
+the terminal should be open in your go code folder.
---
+++ src/main/main.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+func main()  {
+	fmt.Println("Hello, world!");
+}
+
+//you can run this go code in your terminal writing [go run src\main\main.go]. 
+//the terminal should be open in your go code folder.
